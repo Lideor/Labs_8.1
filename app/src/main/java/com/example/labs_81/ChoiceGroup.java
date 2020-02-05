c5477c230d2e32b2405ec06136156d6431ac409e
Ikar <Lideor@yandex.ru>
2020-02-05 22:32:21 +0300

    В книге мой жизни давно засалены страницы

---
+++ app/src/main/java/com/example/labs_81/ChoiceGroup.java
@@ -0,0 +1,14 @@
+package com.example.labs_81;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoiceGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_group);
+    }
+}
