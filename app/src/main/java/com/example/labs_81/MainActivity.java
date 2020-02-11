e349b0b4acd20ebfbf99ed1eeaac54aa7524d8f9
Ikar <Lideor@yandex.ru>
2020-02-11 13:55:33 +0300

    Навсякий случй

--- app/src/main/java/com/example/labs_81/MainActivity.java
+++ app/src/main/java/com/example/labs_81/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mActionBarToolbar);
 
